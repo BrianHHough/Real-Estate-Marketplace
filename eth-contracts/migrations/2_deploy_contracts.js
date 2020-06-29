c81559511a40c5d4de2f77b40b455d7d7a0a42c6
BrianHHough <brianhoughmedia@gmail.com>
2020-06-28 20:10:11 -0400

    truffle migrate works successfully!!

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,11 +1,12 @@
 // migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+// var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+var SquareVerifier = artifacts.require("Verifier");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-// var ERC721Mintable = artifacts.require("IntREstellarToken");
+var ERC721Mintable = artifacts.require("IntREstellarToken");
 
 module.exports = function(deployer) {
   //deployer.deploy(SquareVerifier);
   //deployer.deploy(SolnSquareVerifier);
-  deployer.deploy(SquareVerifier).then(() => deployer.deploy(SolnSquareVerifier, SquareVerifier.address)
-  );
+  deployer.deploy(SquareVerifier).then(() => { return deployer.deploy(SolnSquareVerifier, SquareVerifier.address)
+  });
 };
